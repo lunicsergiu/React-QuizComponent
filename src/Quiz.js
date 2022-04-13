f7da7c3927b3d787243a959a9a11d311ea9e507b
lunicsergiu <lunicsergiu1999@gmail.com>
2022-04-13 13:03:01 +0200

    forth module done

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+    constructor(props){
+        super(props)
+        this.state= {quiz_position:1}
+    }
   render() {
     console.log(quizData.quiz_questions[0].instruction_text);
-    console.log(quizData.quiz_questions[0]);
-
+    console.log(quizData.quiz_questions.length);
+    const isQuizEnd =
+      ((this.state.quiz_position-1)) === quizData.quiz_questions.length;
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />
+        )}
       </div>
     );
   }
