b147bb9b136131d45021e19d4c9f24c1ebf81ed3
lunicsergiu <lunicsergiu1999@gmail.com>
2022-04-13 15:14:22 +0200

    sixth module done

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
+
   handleClick = (buttonText) => {
-      if (buttonText === this.props.quiz_question.answer ) {
-          this.props.showNextQuestionHandler()
-      }
+    if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
+      this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
+    }
   };
 
   render() {
@@ -16,17 +24,22 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.quiz_question.answer_options.map((answer_option, index) => {
-              return (
-                <QuizQuestionButton
-                  key={index}
-                  button_text={answer_option}
-                  clickHandler={this.handleClick.bind(this)}
-                />
-              );
-            })}
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    key={index}
+                    button_text={answer_option}
+                    clickHandler={this.handleClick.bind(this)}
+                  />
+                );
+              }
+            )}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right</p>
+        ) : null}
       </main>
     );
   }
